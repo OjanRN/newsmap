f41fcb4a543a86979b90b23ce78b0f7d5067eb7a
OjanRN <ojanrn5@gmail.com>
2022-12-29 14:44:32 +0700

    deleted newsmap.py

--- code/newsmap.py
+++ code/newsmap.py
@@ -1,8 +1,14 @@
-import requests
-import json
+import requests, json, time
 
 def main():
-    mainApi = requests.get("https://newsapi.org/")
-    print(mainApi.content)
+    print("Searching for articles\n")
+    r = requests.get("https://newsapi.org/v2/everything?q=Crypto&apiKey=539c855342ad4d198670f6859e60f978")
+    mainContent = r.json()
+    for content in mainContent['articles']:
+        print(content['title'], end='\r')
+        if "crash" in content['description']:
+            print("")
+        time.sleep(0.01)
+
 
 print(main())
